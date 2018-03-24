b6876003c51fb2b32f4b6bc2296e987339341ad8
Tim Pietrusky <timpietrusky@gmail.com>
2018-03-24 16:44:42 +0100

    refactor(everything): Stuff

--- src/index.js
+++ src/index.js
@@ -76,91 +76,79 @@ const baseStyles = css`
   ${'' /* --font-size: calc(var(--min-font-size) * 1px + (var(--max-font-size) - var(--min-font-size)) * calc(1vw + 1vh); */};
 `
 
+const slides = [
+      cloneElement(slide.worksInMyBrowser, {key: uuid()}),
+      slide.predictTheFuture,
+      slide.inTheNext30s,
+      slide.livePerformance,
+      slide.everythingAtTheSameTime,
+      slide.timeTravelTheories,
+      slide.travelInTime,
+
+      /* Maybe this is too much in general? Just show the video of the end, but that's it? */,
+      cloneElement(slide.timeTraveling, {key: uuid()}),
+      /* Should we add more here? Do we switch over to my NoteBook for the performance? */,
+
+      slide.gregorAdamsTimPietrusky,
+      slide.bestWayToPredictTheFutureIsToCreateIt,
+
+      slide.backgroundGregor,
+      slide.drawingsFromGregor,
+
+      slide.backgroundTim,
+      slide.flashingLights,
+      slide.familyTim,
+      slide.nerddisco,
+      slide.jsconfeu2014,
+      slide.jsconfeu2014PerformanceTim,
+      slide.jsconfeu2014FirstContactWithGregor,
+      slide.newYearsEve2017,
+      slide.newYearsEve2017VjAndDjDance,
+      slide.newYearsEve2017PerformanceVideo,
+      slide.loveLightsTim,
+
+      slide.letsDoACollaborationTalk,
+
+      slide.luminave,
+      slide.webmidi,
+      slide.novationLaunchpadMini,
+      slide.webmidiCode,
+      slide.dmx512,
+      slide.newYearsEve2017Performance,
+      slide.dmx512Network,
+      slide.dmx512Plug,
+      slide.dmx512LightForJsfestExplained,
+      slide.dmx512Manual,
+      slide.dmx512HowToControlIt,
+      slide.webusb,
+      slide.webusbDmxControllerConcept,
+      slide.webusbNotAsEasyAsYouThink,
+      slide.webusbCustomDevice,
+      slide.webusbArduino,
+      slide.webusbArduinoDmxShield,
+      slide.webusbArduinoSketch,
+      slide.webusbArduinoSketchIncludeAndDefine,
+      slide.webusbArduinoSketchSetup,
+      slide.webusbArduinoSketchLoop,
+      slide.webusbArduinoSketchUploadDone,
+      slide.webusbDmxController,
+      slide.webusbDmx512Data,
+      slide.luminaveDemo,
+      slide.webusbArduinoHowDoesItWork,
+
+      slide.dekkProject,
+
+      slide.tryAllTheThings,
+      slide.livePerformance,
+      slide.thankYou
+]
+
+
 const App = () => (
   <Deck mixin={baseStyles}>
     {elements}
     {plugins}
-
-    {cloneElement(slide.worksInMyBrowser, {key: uuid()})}
-
-    {slide.predictTheFuture}
-
-    {slide.inTheNext30s}
-
-    {slide.livePerformance}
-
-    {slide.everythingAtTheSameTime}
-    {slide.timeTravelTheories}
-    {slide.travelInTime}
-
-    {/* Maybe this is too much in general? Just show the video of the end, but that's it? */}
-    {cloneElement(slide.timeTraveling, {key: uuid()})}
-    {/* Should we add more here? Do we switch over to my NoteBook for the performance? */}
-
-    {slide.gregorAdamsTimPietrusky}
-    {slide.bestWayToPredictTheFutureIsToCreateIt}
-
-    {slide.backgroundGregor}
-    {slide.drawingsFromGregor}
-
-    {slide.backgroundTim}
-    {slide.flashingLights}
-    {slide.familyTim}
-    {slide.nerddisco}
-    {slide.jsconfeu2014}
-    {slide.jsconfeu2014PerformanceTim}
-    {slide.jsconfeu2014FirstContactWithGregor}
-    {slide.newYearsEve2017}
-    {slide.newYearsEve2017VjAndDjDance}
-    {slide.newYearsEve2017PerformanceVideo}
-    {slide.loveLightsTim}
-
-    {/*   But I came to the conculsion that what I can do best
-          Is to combine exsiting software with each other */}
-    {/* {slide.manyDifferentProjectsTim} */}
-
-    {/* pre-analyze the music for audio control */}
-
-    {slide.letsDoACollaborationTalk}
-
-    {slide.luminave}
-    {slide.webmidi}
-    {slide.novationLaunchpadMini}
-    {slide.webmidiCode}
-    {slide.dmx512}
-    {slide.newYearsEve2017Performance}
-    {slide.dmx512Network}
-    {slide.dmx512Plug}
-    {slide.dmx512LightForJsfestExplained}
-    {slide.dmx512Manual}
-    {slide.dmx512HowToControlIt}
-    {slide.webusb}
-    {slide.webusbDmxControllerConcept}
-    {slide.webusbNotAsEasyAsYouThink}
-    {slide.webusbCustomDevice}
-    {slide.webusbArduino}
-    {slide.webusbArduinoDmxShield}
-    {slide.webusbArduinoSketch}
-    {slide.webusbArduinoSketchIncludeAndDefine}
-    {slide.webusbArduinoSketchSetup}
-    {slide.webusbArduinoSketchLoop}
-    {slide.webusbArduinoSketchUploadDone}
-    {slide.webusbDmxController}
-    {slide.webusbDmx512Data}
-    {slide.luminaveDemo}
-    {slide.webusbArduinoHowDoesItWork}
-
-    {slide.dekkProject}
-
-    {slide.code}
-
-    {slide.inTheNext30sClapHands}
-
-    {slide.tryAllTheThings}
-
-    {slide.livePerformance}
-
-    {slide.thankYou}
+    {slides}
   </Deck>
 )
 
